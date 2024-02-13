8d08e99538bcf4887d9e41c253c11186a9bc22d2
SENECHAL Louis <130775686+LudwigvonSchlett@users.noreply.github.com>
2024-02-13 11:24:51 +0100

    Classes de bases

--- Java_Rectangle/src/form/Element.java
+++ Java_Rectangle/src/form/Element.java
@@ -1,5 +1,23 @@
 package form;
 
-public abstract class Element {
+import java.io.Serializable;
+
+public abstract class Element implements Serializable{
+	
+	public abstract void saveBinary(String filename) throws Exception;
+	
+	public abstract void loadBinary(String filename) throws Exception;
+	
+	public abstract String toString();
+	
+	public abstract void move();
+	
+	public abstract void delete();
+	
+	public abstract void union();
+	
+	public abstract void intersect();
+	
+	public abstract void difference();
 
 }
---
+++ Java_Rectangle/src/form/Form.java
@@ -0,0 +1,53 @@
+package form;
+
+public class Form extends Element{
+
+	@Override
+	public void saveBinary(String filename) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void loadBinary(String filename) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void union() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void intersect() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void difference() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
