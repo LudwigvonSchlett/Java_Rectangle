50a047d4de66276cd2dec2e42b00ccb16c646c62
SENECHAL Louis <130775686+LudwigvonSchlett@users.noreply.github.com>
2024-04-21 17:21:14 +0200

    Update Window.java

--- Java_Rectangle/src/simpleform/Window.java
+++ Java_Rectangle/src/simpleform/Window.java
@@ -20,8 +20,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
 import java.awt.event.ActionEvent;
 import javax.swing.JPanel;
 import javax.swing.JToolBar;
